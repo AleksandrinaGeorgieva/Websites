9625e8c4f98b9d350158942cc0ebf531d1e1b426
AleksandrinaGeorgieva <alex.dm1@gmail.com>
2017-03-10 14:25:07 +0200

    first commit

---
+++ GalMaria/scripts/docready.js
@@ -0,0 +1,28 @@
+$(function(){
+    $("#footer").load("footer.html");
+    $("#header").load("header.html");
+    if($('.gallery')){
+        $('.gallery').vitGallery();
+        $('.gallery').vitGallery({
+            buttons: true,
+            galleryHeight: 'auto',
+            imgBlockClass: 'gallery__img-block',
+            controls: 'thumbnails', // points thumbnails
+            controlsClass: 'gallery__controls',
+            thumnailWidth: 90,
+            thumnaiHeight: 60,
+            thumbnailMargin: 5,
+            animateSpeed: 1000,
+            description: true,
+            imgPadding: 15,
+            autoplay: false,
+            autoplayDelay: 3000,
+            fullscreen: false,
+            transition: 'slide' // slide crossfade
+        });
+    }
+});
+
+$(document).ready(function(){
+    var $gallery = $('.gallery');
+});
